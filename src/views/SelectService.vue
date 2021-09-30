0e028bb7441be651df7bacb6d0d2960f284a391a
theyokky <Yanakorn.j@ku.th>
2021-10-01 01:53:43 +0700

    edit SelectService page

--- src/views/SelectService.vue
+++ src/views/SelectService.vue
@@ -1,114 +1,224 @@
 <template>
   <v-container>
-    <v-row style="margin-top:1vh">
-      <h3>บริการ : ทำผม ทั้งหมด 1 ประเภท</h3>
-    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <div style="header">
+          <span>หมวดหมู่</span>
+          <h2 style="text-align: right">ชื่อหมวดหมู่</h2>
+        </div>
+        <div
+          class="type_select_btn"
+          v-for="(v, index) in category_lists"
+          :key="index"
+          :class="{ active: index == lists_select }"
+          @click="lists_select = index"
+        >
+          {{ v.type_label }}
+        </div>
+      </v-col>
 
-    <v-row style="margin-top:1vh">
-      <v-spacer> </v-spacer>
+      <v-col>
+        <v-row>
+          <v-col>
+            <img :src="data.image[0]" style="width: 100%" />
+          </v-col>
+          <v-col>
+            <v-row>
+              <h2>{{ data.title }}</h2>
+              <p>
+                {{ data.desc }}
+              </p>
+            </v-row>
+            <v-row>
+              <v-col
+                v-for="(v, index) in data.image.slice(1, data.image.length)"
+                :key="index"
+              >
+                <img :src="v" style="width: 100%" />
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
 
-      <v-col cols="3" style="background:grey;">
-        <h3 style="text-align: center;">ประเภทบริการ (0)</h3>
-        <v-card flat>
-          <v-card-text>
-            <v-container fluid>
-              <v-row>
-                <v-col>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="ทรีทเมนท์"
-                    hide-details
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="แต่งหน้า"
-                    hide-details
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="สปา"
-                    hide-details
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="สระผม"
-                    hide-details
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="นวด"
-                    hide-details
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="ex4"
-                    label="ทำเล็บ"
-                    hide-details
-                  ></v-checkbox>
-                </v-col>
+        <v-row>
+          <v-col
+            cols="12"
+            style="background: #f1f1f1; margin-bottom: 5px"
+            v-for="(v, index) in data.services"
+            :key="index"
+          >
+            <v-row class="align-center">
+              <v-col cols="8">
+                <h2>{{ v.label }}</h2>
+                <h3>เวลาที่ใช้ {{ v.time }} นาที</h3>
+              </v-col>
+              <v-col>
+                <v-row class="align-center justify-end">
+                  <b>฿ {{ v.price }}</b>
+                  <button class="buy_btn">ซื้อคูปอง</button>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <p style="margin-top: 10px">รีวิวจากผู้ใช้บริการล่าสุด 3 ท่าน</p>
+        </v-row>
+
+        <v-row>
+          <v-row>
+            <v-col
+              cols="3.5"
+              style="
+                background: #979292;
+                margin-right: 5px;
+                margin-left: 5px;
+                margin-bottom: 10px;
+              "
+              v-for="(v, index) in data_review_lists"
+              :key="index"
+              class="white--text"
+            >
+              <v-row
+                style="padding: 1vh; padding-left: 3vh"
+              >
+                {{ v.name_customer }}
               </v-row>
-
-              <v-container style="text-align: center; ">
-                <v-btn text style="background:#41ad69;" class="white--text">
-                  Apply filters
-                </v-btn>
-              </v-container>
-            </v-container>
-          </v-card-text>
-        </v-card>
-      </v-col>
-
-      <v-spacer> </v-spacer>
-
-      <v-col cols="8" style="background:grey;">
-        <v-img
-          lazy-src="https://picsum.photos/id/11/10/6"
-          max-height="300"
-          max-width="300"
-          src="https://picsum.photos/id/11/500/300"
-        ></v-img>
-
-        <v-row style="margin-top:1vh;">
-          <v-col col="3">
-            <v-card color="#385F73" dark style="margin-top=10px">
-              <v-card-title text-color="red" class="text-h6">
-                สระไดร์ธรรมดา
-                <v-card-title text-color="red" class="red--text">
-                  $ราคา
-                </v-card-title>
-              </v-card-title>
-              <v-card-subtitle>เวลาที่ใช้</v-card-subtitle>
-              <v-container style="text-align:center; ">
-                <v-btn text style="background:#41ad69; " class="white--text">
-                  ซื้อคูปอง
-                </v-btn>
-              </v-container>
-            </v-card>
-          </v-col>
-        </v-row>
-
-        <v-row style="margin-top:1vh;">
-          <v-col col="3">
-            <v-card color="#385F73" dark style="margin-top=10px">
-              <v-card-title text-color="red" class="text-h6">
-                สระแบบผ่อนคลาย 90 นาที
-                <v-card-title text-color="red" class="red--text">
-                  $ราคา
-                </v-card-title>
-              </v-card-title>
-              <v-card-subtitle>เวลาที่ใช้</v-card-subtitle>
-              <v-container style="text-align:center; ">
-                <v-btn text style="background:#41ad69; " class="white--text">
-                  ซื้อคูปอง
-                </v-btn>
-              </v-container>
-            </v-card>
-          </v-col>
+              <v-row 
+                class="align-center; justify-center" 
+                style="padding: 2vh">
+                {{ v.review_desc }}
+              </v-row>
+              <v-row
+                class="align-center; justify-end"
+                style="padding: 1vh; padding-right: 3vh"
+              >
+                {{ v.service_customer }}
+              </v-row>
+            </v-col>
+          </v-row>
         </v-row>
       </v-col>
-
-      <!-- <v-spacer></v-spacer> -->
     </v-row>
   </v-container>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      category_name: "salon",
+      category_label: "ซาลอน",
+      lists_select: 0,
+      category_lists: [
+        {
+          type_name: "cut",
+          type_label: "บริการตัดผม",
+        },
+        {
+          type_name: "dyehair",
+          type_label: "บริการย้อมผม",
+        },
+        {
+          type_name: "washhair",
+          type_label: "บริการสระผม",
+        },
+        {
+          type_name: "treatmenthair",
+          type_label: "บริการทรีทเม้นต์เส้นผม",
+        },
+      ],
+      data: {
+        title: "",
+        desc: "",
+        image: [],
+        services: [],
+      },
+      data_review_lists: [
+        {
+          name_customer: "yok",
+          review_desc:
+            "first and yok love bankky so much manow have house" +
+            "that have many of cats too such as bunroad",
+          service_customer: "สระผม",
+        },
+        {
+          name_customer: "first",
+          review_desc: "dsfsdfsedfesdfsdfs",
+          service_customer: "ตัดผม",
+        },
+        {
+          name_customer: "manow",
+          review_desc: "dsfsdfsedfesdfsdf",
+          service_customer: "ย้อมผม",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.getListData("cut");
+  },
+  methods: {
+    getListData(type) {
+      if (type == "cut") {
+        //api request
+        //api response
+        this.data = {
+          title: "บริการตัดผม",
+          desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque" +
+            "voluptatem unde architecto fuga laboriosam enim quo inventore" +
+            "corrupti perferendis sit. Similique libero eveniet aliquid" +
+            "inventore architecto culpa optio temporibus odio.",
+          image: [
+            "https://i.imgur.com/A4R3m2l.jpeg",
+            "https://i.imgur.com/A4R3m2l.jpeg",
+            "https://i.imgur.com/A4R3m2l.jpeg",
+            "https://i.imgur.com/A4R3m2l.jpeg",
+          ],
+          services: [
+            {
+              label: "โกนผม",
+              time: 15,
+              price: 1000,
+            },
+            {
+              label: "ปลูกผม",
+              time: 15,
+              price: 1500,
+            },
+          ],
+        };
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.type_select_btn {
+  padding: 1vh;
+  border-radius: 4px;
+  margin-bottom: 5px;
+  transition: 0.5s;
+  cursor: pointer;
+}
+
+.type_select_btn:hover {
+  background: #d8d8d8;
+}
+
+.active {
+  background: #f1f1f1;
+}
+
+.buy_btn {
+  margin-left: 5vh;
+  margin-right: 2vh;
+  padding: 1vh;
+  padding-left: 2vh;
+  padding-right: 2vh;
+  background: #2bd598;
+}
+</style>
