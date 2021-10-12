90fcb5e0038c27fd3f1af56b2a01e81356d97f52
tataPongpol <pongpol.ro@ku.th>
2021-10-12 13:54:55 +0700

    connect api in payment_record

--- src/components/customer/payment_record.vue
+++ src/components/customer/payment_record.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row>
+    <!-- <v-row>
       <v-col>
         <span style="font-size: 40px">ประวัติการชำระเงินของฉัน</span>
         <v-divider></v-divider>
@@ -66,48 +66,82 @@
         </v-col>
 
       </v-col>
+    </v-row> -->
+
+    <v-row>
+      <v-col>
+        <h1>ประวัติการชำระเงินของฉัน</h1>
+        <v-data-table :headers="headers" :items="record_payment_list">
+        </v-data-table>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import AuthUser from "@/store/AuthUser";
 export default {
   data() {
     return {
-      record_payment_list: [
-        {
-          date: "10-9-2021",
-          category_lists: [
-            {
-              coupon_name: "สระไดร์+ตัดผม",
-              count: 2,
-            },
-            {
-              coupon_name: "ทาเล็บ",
-              count: 1,
-            },
-          ],
-        },
-
-        {
-          date: "12-9-2021",
-          category_lists: [
-            {
-              coupon_name: "สปา",
-              count: 3,
-            },
-            {
-              coupon_name: "นวดคอ บ่า ไหล่",
-              count: 1,
-            },
-            {
-              coupon_name: "สระไดร์",
-              count: 2,
-            },
-          ],
-        },
+      headers: [
+        { text: "ชื่อ Coupon", value: "coupon.name" },
+        { text: "ซื้อเมื่อ", value: "created_at" },
+        { text: "จำนวน", value: "count" },
       ],
+      record_payment_list: []
+      // record_payment_list: [
+      //   {
+      //     date: "10-9-2021",
+      //     category_lists: [
+      //       {
+      //         coupon_name: "สระไดร์+ตัดผม",
+      //         count: 2,
+      //       },
+      //       {
+      //         coupon_name: "ทาเล็บ",
+      //         count: 1,
+      //       },
+      //     ],
+      //   },
+
+      //   {
+      //     date: "12-9-2021",
+      //     category_lists: [
+      //       {
+      //         coupon_name: "สปา",
+      //         count: 3,
+      //       },
+      //       {
+      //         coupon_name: "นวดคอ บ่า ไหล่",
+      //         count: 1,
+      //       },
+      //       {
+      //         coupon_name: "สระไดร์",
+      //         count: 2,
+      //       },
+      //     ],
+      //   },
+      // ],
     };
+  },
+  methods: {
+    
+  },
+  created() {
+    const token = AuthUser.getters.user.api_token
+
+    this.$http
+      .get("http://127.0.0.1:8000/api/payment_record/", {
+        headers: { Authorization: `${token}` },
+      })
+      .then((response) => {
+        if (response.status == 200) {
+          this.record_payment_list = response.data;
+          console.log(this.record_payment_list);
+        } else {
+          console.log(response.error);
+        }
+      });
   },
 };
 </script>
